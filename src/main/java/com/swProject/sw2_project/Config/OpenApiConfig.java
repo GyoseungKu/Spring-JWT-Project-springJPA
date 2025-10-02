064ac208caf9e8d392d21034c519bd083176e464
Gyoseung Ku <ku@gyoseung.me>
2025-10-02 10:19:08 +0900

    Swagger설정 추가

---
+++ src/main/java/com/swProject/sw2_project/Config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.swProject.sw2_project.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Sw2 API", version = "v1"),
+        servers = {
+                @Server(url = "https://gyoseung.me", description = "Production Server"),
+                @Server(url = "http://127.0.0.1:9090", description = "Local Server")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+public class OpenApiConfig {}
