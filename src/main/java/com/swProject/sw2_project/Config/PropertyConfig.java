97346d878aec663ef42152691b197ec6f06c7abd
Gyoseung Ku <ku@gyoseung.me>
2025-10-02 10:53:36 +0900

    PropertyConfig 추가

---
+++ src/main/java/com/swProject/sw2_project/Config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.swProject.sw2_project.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:env.properties")
+
+public class PropertyConfig {
+}
